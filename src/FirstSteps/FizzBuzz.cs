d3037a55025df7a7a40a8cab865c25c27fca7680
siudzinski <tomek.siudzinski@gmail.com>
2022-07-29 20:18:02 +0200

    add fizzbuzz tips

--- src/FirstSteps/FizzBuzz.cs
+++ src/FirstSteps/FizzBuzz.cs
@@ -6,6 +6,19 @@ namespace FirstSteps
     {
         public string[] GetWords(int numberOfWords)
         {
+            //crete an array of strings of the given size
+
+            //use "for" loop to iterate through the array
+            
+                //if the number is divisible by 3 and 5, set the word to "FizzBuzz"
+
+                //if the number is divisible by 3, set the word to "Fizz"
+
+                //if the number is divisible by 5, set the word to "Buzz"
+
+                //if the number is not divisible by 3 or 5, set the word to the number
+        
+            //return the array of strings
             return Array.Empty<string>();
         }
     }
