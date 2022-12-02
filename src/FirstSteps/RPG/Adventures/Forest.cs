fbba551ef89c8b3d5ab2adbc895c16db63e979b2
Michał <miszalaty@gmail.com>
2022-12-02 14:13:01 +0100

    as always i need some tips ;p

--- src/FirstSteps/RPG/Adventures/Forest.cs
+++ src/FirstSteps/RPG/Adventures/Forest.cs
@@ -7,15 +7,29 @@ namespace FirstSteps.RPG.Adventures
 {
     public class Forest
     {
-        public Forest() { }
-
-        public void Enter(Elf elf)
+        private Elf elf; // Despite i use class Elf value of elf is equals null,
+                         // I think it's rly easy but i can't find a simple way to fix it 
+                         // although i had a similar example before but i can't remember. 
+                         // When i created a new Elf, result was weird. I think i made a elf twice but still add a 10hp more. 
+        public Forest()
         {
-            Console.WriteLine("You enetered the old forest. What do u want to do?");
+   
+        }
+
+        public void Enter(Hero _hero)
+        {
+            if (_hero is Elf)
+            {
+                Console.WriteLine("You enetered the old forest. What do u want to do?");
+            }
+            else
+            {
+                Console.WriteLine("You can't enter to the forest! Only Elf can enter!");
+                return;
+            }      
             Console.WriteLine("Type 'heal' to increase your healt.");
             Console.WriteLine("Type 'collect ' to collect a random number of arrows");
             var userInput = Console.ReadLine();
-
             switch (userInput)
             {
                 case "heal":
@@ -29,7 +43,7 @@ namespace FirstSteps.RPG.Adventures
                         collectedArrows.Add(new Arrow());
                     }
                     elf.CollectArrows(collectedArrows);
-
+                    Console.WriteLine($" You gain:  { arrowsNumber} arrows!");
                     break;
                 default:
                     Console.WriteLine("Wrong type!");
--- src/FirstSteps/RPG/Heroes/Elf.cs
+++ src/FirstSteps/RPG/Heroes/Elf.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 
+
 namespace FirstSteps.RPG.Heroes
 {
     public class Elf : Hero, IGreeting
