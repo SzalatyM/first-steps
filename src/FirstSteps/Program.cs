90952ba5e5dee78c51c9ce05811d37fd61b957bc
Michał <miszalaty@gmail.com>
2023-01-09 21:00:51 +0100

    while loop (problem)

--- src/FirstSteps/Program.cs
+++ src/FirstSteps/Program.cs
@@ -8,7 +8,7 @@ namespace FirstSteps
     {
         static void Main(string[] args)
         {
-
+            var user = Console.ReadLine();
             AnsiConsole.MarkupLine($"{Emoji.Known.BowAndArrow} [darkgreen]Welcome to the RPG game [/] {Emoji.Known.CrossedSwords}");
 
             Game.CreateHero();
@@ -16,25 +16,17 @@ namespace FirstSteps
             AnsiConsole.MarkupLine($"{Emoji.Known.Dagger} [red]Let's play the game [/] {Emoji.Known.Dagger}");
 
             Console.WriteLine("Type 'end' if you want to quit.");
-            Console.WriteLine("\nIf You want go back to the previous menu press 'x'\n");
+            Console.WriteLine("\nIf You want go back to the previous menu press button\n");
             Console.WriteLine("________________________________");
-            string command = AnsiConsole.Prompt(
-            new SelectionPrompt<string>().Title("\nSelect what you want to do: ").AddChoices("stats", "treasure", "inventory", "forest", "dungeons", "mine"));
-
-            Console.WriteLine($"\nYou choose: { command}");
-            Game.HandleCommand(command);         
             do
             {
-               
-                var userCommand = Console.ReadLine();
-                if (userCommand == "x") // I try to do this yet but i think I need some prompt about it 
-                {
-                    Game.HandleCommand(command);
-                }
-                Game.HandleCommand(userCommand);
-                Console.WriteLine();               
+                string command = AnsiConsole.Prompt(
+            new SelectionPrompt<string>().Title("\nSelect what you want to do: ").AddChoices("stats", "treasure", "inventory", "forest", "dungeons", "mine"));
+                Game.HandleCommand(command);
+                Console.ReadLine();  
+                Console.WriteLine();
             }
-            while (command.ToLower() != "end");
+            while (Game.HandleCommand(user) != "end"); // I've had this before like u wrote, but I just had a problem with a while loop so i decided to change conception :D
         }
     }
 }
