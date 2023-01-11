50f00c8a6fbfa25cd9533951b4447b6ae586d338
Michał <miszalaty@gmail.com>
2023-01-11 22:08:01 +0100

    fixed loop (do while)

--- src/FirstSteps/Program.cs
+++ src/FirstSteps/Program.cs
@@ -8,25 +8,25 @@ namespace FirstSteps
     {
         static void Main(string[] args)
         {
-            var user = Console.ReadLine();
             AnsiConsole.MarkupLine($"{Emoji.Known.BowAndArrow} [darkgreen]Welcome to the RPG game [/] {Emoji.Known.CrossedSwords}");
 
             Game.CreateHero();
 
             AnsiConsole.MarkupLine($"{Emoji.Known.Dagger} [red]Let's play the game [/] {Emoji.Known.Dagger}");
 
-            Console.WriteLine("Type 'end' if you want to quit.");
+            Console.WriteLine("\nType 'end' if you want to quit.");
             Console.WriteLine("\nIf You want go back to the previous menu press button\n");
             Console.WriteLine("________________________________");
+            string userInput;
             do
             {
                 string command = AnsiConsole.Prompt(
             new SelectionPrompt<string>().Title("\nSelect what you want to do: ").AddChoices("stats", "treasure", "inventory", "forest", "dungeons", "mine"));
                 Game.HandleCommand(command);
-                Console.ReadLine();  
                 Console.WriteLine();
+                userInput = Console.ReadLine();
             }
-            while (Game.HandleCommand(user) != "end"); // I've had this before like u wrote, but I just had a problem with a while loop so i decided to change conception :D
+            while (userInput != "end");
         }
     }
 }
