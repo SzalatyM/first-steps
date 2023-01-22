984724e0c8c0c670739d96e1036c5abcb01329eb
siudzinski <tomek.siudzinski@gmail.com>
2023-01-22 16:37:22 +0100

    add empty MineTests class

---
+++ src/FirstSteps.Tests/RPG/Adventures/MineTests.cs
@@ -0,0 +1,12 @@
+using FirstSteps.RPG.Adventures;
+using FirstSteps.RPG.Heroes;
+using FluentAssertions;
+using Xunit;
+
+namespace FirstSteps.Tests.RPG.Adventures
+{
+    public class MineTests
+    {
+        
+    }
+}
