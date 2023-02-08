b3b1051fc52bc83c3287f438ad240a20a8d3f0ae
Michał <miszalaty@gmail.com>
2023-02-08 11:43:18 +0100

    complete test for the mine class.

--- src/FirstSteps.Tests/RPG/Adventures/MineTests.cs
+++ src/FirstSteps.Tests/RPG/Adventures/MineTests.cs
@@ -7,7 +7,6 @@ namespace FirstSteps.Tests.RPG.Adventures
 {
     public class MineTests
     {
-        public int coins;
         [Fact]
 
         public void Dwarf_Should_Gain_One_Coin_After_the_First_Visit__In_The_Mine()
@@ -18,6 +17,26 @@ namespace FirstSteps.Tests.RPG.Adventures
             mine.Enter(dwarf);
 
             dwarf.Coins.Should().Be(1);
+        }
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(4, 2)]
+        [InlineData(5, 3)]
+        [InlineData(8, 4)]
+        [InlineData(9, 5)]
+        [InlineData(12, 6)]
+        public void Dwarf_Should_Gain_Three_Coins_When_Numbers_Are_Odd_Otherwise_Should_Gain_One_Coin(int expectedCoins, int visitCounter)
+        {
+
+            var mine = new Mine();
+            var dwarf = new Dwarf("Testhero");
+
+            for (int i = 0; i < visitCounter; i++)
+            {
+                mine.Enter(dwarf);
+            }
+
+            dwarf.Coins.Should().Be(expectedCoins);
         }
     }
 }
