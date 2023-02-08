ea78dd2c48d9e77e1e5b712c6d6ac5a2246cb1f6
Michał <miszalaty@gmail.com>
2023-02-08 10:16:09 +0100

    second method deleted.

--- src/FirstSteps.Tests/RPG/Adventures/MineTests.cs
+++ src/FirstSteps.Tests/RPG/Adventures/MineTests.cs
@@ -9,7 +9,7 @@ namespace FirstSteps.Tests.RPG.Adventures
     {
         public int coins;
         [Fact]
-        
+
         public void Dwarf_Should_Gain_One_Coin_After_the_First_Visit__In_The_Mine()
         {
             var mine = new Mine();
@@ -18,20 +18,6 @@ namespace FirstSteps.Tests.RPG.Adventures
             mine.Enter(dwarf);
 
             dwarf.Coins.Should().Be(1);
-
-        }
-        [Theory]
-        [InlineData(1,1)]
-
-
-        public void Dwarf_Should_Get_One_Coin_After_First_Visit_In_The_Mine(int expectedResult, int firstVisit)
-        {
-            var mine = new Mine();
-            var dwarf = new Dwarf("Testhero");
-
-            mine.Enter(dwarf);
-            firstVisit += 1;
-            dwarf.Coins.Should().Be(expectedResult);
         }
     }
 }
