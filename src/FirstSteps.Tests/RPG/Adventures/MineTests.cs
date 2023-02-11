16e7f8a5be7817e9aafbcfffd44f5a4bab59bad5
Michał <109979901+SzalatyM@users.noreply.github.com>
2023-02-11 18:50:03 +0100

    First test for the mine (#33)
    * First test for mine
    
    * Mine tests  #works
    
    * second method deleted.
    
    * complete test for the mine class.
    
    * first test for the mine.

--- src/FirstSteps.Tests/RPG/Adventures/MineTests.cs
+++ src/FirstSteps.Tests/RPG/Adventures/MineTests.cs
@@ -7,6 +7,16 @@ namespace FirstSteps.Tests.RPG.Adventures
 {
     public class MineTests
     {
-        
+        [Fact]
+
+        public void Dwarf_Should_Gain_One_Coin_After_the_First_Visit__In_The_Mine()
+        {
+            var mine = new Mine();
+            var dwarf = new Dwarf("Test");
+
+            mine.Enter(dwarf);
+
+            dwarf.Coins.Should().Be(1);
+        }
     }
 }
