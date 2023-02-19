c0c9b473afc69167c83ec0b0111f773042877717
Michał <miszalaty@gmail.com>
2023-02-19 11:57:09 +0100

    Fixed test + mine class

--- src/FirstSteps.Tests/RPG/Adventures/MineTests.cs
+++ src/FirstSteps.Tests/RPG/Adventures/MineTests.cs
@@ -23,35 +23,14 @@ namespace FirstSteps.Tests.RPG.Adventures
         [InlineData(4, 2)]
         [InlineData(5, 3)]
         [InlineData(8, 4)]
-        [InlineData(9, 5)]
-        [InlineData(12, 6)]
-        public void Dwarf_Should_Gain_Three_Coins_When_Numbers_Are_Odd_Otherwise_Should_Gain_One_Coin(int expectedCoins, int visitCounter)
-        {
-
-            var mine = new Mine();
-            var dwarf = new Dwarf("Testhero");
-
-            for (int i = 0; i < visitCounter; i++)
-            {
-                mine.Enter(dwarf);
-            }
-
-            dwarf.Coins.Should().Be(expectedCoins);
-        }
-        [Theory]
-        [InlineData(1, 1)]
-        [InlineData(4, 2)]
-        [InlineData(5, 3)]
-        [InlineData(8, 4)]
-        [InlineData(10, 5)]
-        [InlineData(13, 6)]
-        [InlineData(14, 7)]
-        [InlineData(17, 8)]
-        [InlineData(18, 9)]
-        [InlineData(21, 10)]
-        [InlineData(22, 11)]
-        [InlineData(25, 12)]
-        [InlineData(25, 13)]
+        [InlineData(11, 5)]
+        [InlineData(14, 6)]
+        [InlineData(15, 7)]
+        [InlineData(18, 8)]
+        [InlineData(19, 9)]
+        [InlineData(22, 10)]
+        [InlineData(23, 11)]
+        [InlineData(26, 12)]
         public void Dwarf_Should_Gain_Three_Coins_When_Numbers_Are_Odd_Or_Plus_2_Coins_When_Divide_By_Five_When_Number_Is_13_Gain_0_Coins_Otherwise_Gain_1_Coin(int expectedCoins, int visitCounter)
         {
             var mine = new Mine();
--- src/FirstSteps/RPG/Adventures/Mine.cs
+++ src/FirstSteps/RPG/Adventures/Mine.cs
@@ -14,42 +14,65 @@ namespace FirstSteps.RPG.Adventures
                 Display.ErrorText("You can't enter! Only Dwarf can enter to the Mine!");
                 return;
             }
-
             Display.DefaultText("You entered to the Mine! ");
 
             enter += 1;
-
-            var coins = enter % 2 == 0 ? 3 : enter % 5 == 0 ?  +2 : enter == 13 ? 0 : 1;
-          
+            var coins = 1;
+            if (enter % 2 == 0)
+            {
+                coins = 3;
+                DisplayCoins(coins);
+                hero.AddCoins(coins);
+            }
+            else if (enter % 5 == 0)
+            {
+                coins += 2;
+                DisplayCoins(coins);
+                hero.AddCoins(coins);
+            }
+            else if (enter == 13)
+            {
+                coins = 0;
+                DisplayCoins(coins);
+            }
+            else
+            {
+                coins = 1;
+                DisplayCoins(coins);
+                hero.AddCoins(coins);
+            } 
+        }
+        public void DisplayCoins(int coins)
+        {
             Console.WriteLine($"You gain {coins} coins ");
-            hero.AddCoins(coins);
-            Console.WriteLine(hero.Coins);       
-            //int coins;
-            //if (hero is Dwarf)
-            //{
-
-            //    AnsiConsole.MarkupLine($"{Emoji.Known.CrossedSwords} [darkblue]You entered to the Mine![/] {Emoji.Known.CrossedSwords}");
-            //    enter += 1;
-            //}
-            //else
-            //{
-            //    AnsiConsole.MarkupLine($"{Emoji.Known.CrossMarkButton} [red]You can't enter! Only Dwarf can enter to the Mine! [/] {Emoji.Known.CrossMarkButton}");
-            //    return;
-            //}
-
-            //Dwarf dwarf = (Dwarf)hero;
-
-            //if (enter % 2 == 0)
-            //{
-            //     coins = 3;
-            //}
-            //else
-            //{
-            //   coins = 1;
-            //}
-            //Console.WriteLine($"You gain {coins} coins ");
-            //dwarf.AddCoins(coins);
+            Console.WriteLine("________________________");
+            Console.WriteLine($"You currently have {coins} coins");
         }
     }
+    //int coins;
+    //if (hero is Dwarf)
+    //{
+
+    //    AnsiConsole.MarkupLine($"{Emoji.Known.CrossedSwords} [darkblue]You entered to the Mine![/] {Emoji.Known.CrossedSwords}");
+    //    enter += 1;
+    //}
+    //else
+    //{
+    //    AnsiConsole.MarkupLine($"{Emoji.Known.CrossMarkButton} [red]You can't enter! Only Dwarf can enter to the Mine! [/] {Emoji.Known.CrossMarkButton}");
+    //    return;
+    //}
+
+    //Dwarf dwarf = (Dwarf)hero;
+
+    //if (enter % 2 == 0)
+    //{
+    //     coins = 3;
+    //}
+    //else
+    //{
+    //   coins = 1;
+    //}
+    //Console.WriteLine($"You gain {coins} coins ");
+    //dwarf.AddCoins(coins);
 }
 
