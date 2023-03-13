7d61836db31fc7d78c87bc431e0c105e2edbb101
Michał <miszalaty@gmail.com>
2023-03-13 15:51:47 +0100

    2 redundant methods removed

--- src/FirstSteps/RPG/HeroCantHoldItemException.cs
+++ src/FirstSteps/RPG/HeroCantHoldItemException.cs
@@ -7,22 +7,7 @@ namespace FirstSteps.RPG
         const string text = "You can't hold this item";
         public HeroCantHoldItemException() : base(text)
         {
-            
-        }
-       public void TrySpendCoins()
-        {
-            throw new HeroCantHoldItemException();
-        }
-        public void ShowItems()
-        {
-            try
-            {
-                TrySpendCoins();
-            }
-            catch(HeroCantHoldItemException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+
         }
     }
 }
