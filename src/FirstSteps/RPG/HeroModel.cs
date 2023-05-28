2cf2bae2ed3ec0349a9056ec21e09377de8f53e0
Michał <miszalaty@gmail.com>
2023-05-28 13:26:01 +0200

    HeroRepository and HeroModel

---
+++ src/FirstSteps/RPG/HeroModel.cs
@@ -0,0 +1,19 @@
+﻿using FirstSteps.RPG.Heroes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstSteps.RPG
+{
+    public class HeroModel
+    {       
+        public string _name;
+        public Races _race;
+        public int _strength;
+        public int _intelligence;
+        public int _agility;
+        public int _health;
+        public int _damage;
+        public int _coinsBag;
+    }
+}
---
+++ src/FirstSteps/RPG/HeroesRepository.cs
@@ -0,0 +1,26 @@
+﻿using FirstSteps.RPG.Heroes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace FirstSteps.RPG
+{
+    public class HeroesRepository
+    {
+        const string FilePath = @"C:\Users\misza\source\repos\first-steps\src\FirstSteps\RPG\File\hero.json";
+
+        public static void SafeHero(Hero hero)
+        {
+            string serialize = JsonSerializer.Serialize(hero);
+            File.WriteAllText(FilePath, serialize);
+        }
+        public static void LoadHero()
+        {
+            string deSerialize = File.ReadAllText(FilePath);
+            HeroModel heroModel = JsonSerializer.Deserialize<HeroModel>(deSerialize);
+            Console.WriteLine(heroModel);
+        }
+    }
+}
