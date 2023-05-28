8fadadf2ee50963b564aeb151ec29d1db33ee2da
Michał <miszalaty@gmail.com>
2023-05-28 12:36:55 +0200

    new classes

---
+++ src/FirstSteps/RPG/HeroModel.cs
@@ -0,0 +1,23 @@
+﻿using FirstSteps.RPG.Heroes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstSteps.RPG
+{
+    public class HeroModel
+    {
+        
+        public string _name;
+        public Races _race;
+         public int _strength;
+        public int _intelligence;
+        public int _agility;
+        protected int _health;
+        public int Health { get { return _health; } }
+        protected int _damage;
+        private int _coinsBag;
+        public int Coins { get { return _coinsBag; } set { _coinsBag = value; } }
+
+    }
+}
