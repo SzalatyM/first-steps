5c849dc48099d7b736789fd817a9b1ace6b6c0fc
Michał <miszalaty@gmail.com>
2023-10-07 12:51:09 +0200

    DealDamage method

--- src/FirstSteps/RPG/Heroes/Boss.cs
+++ src/FirstSteps/RPG/Heroes/Boss.cs
@@ -7,7 +7,7 @@ namespace FirstSteps.RPG.Heroes
     public class Boss : IAttacker
     {
         public string Name { get; private set; }
-        public int MaxHealth { get ; private set; }
+        public int MaxHealth { get; private set; }
         public int Health { get; private set; }
         public int Strength { get; private set; }
         public int Damage { get; private set; }
@@ -19,7 +19,7 @@ namespace FirstSteps.RPG.Heroes
 
         public static Boss CreateDiablo()
         {
-            var diablo = new Boss { Name = "Diablo", Health = 40, Strength = 6, Damage = 6 };
+            var diablo = new Boss { Name = "Diablo", Health = 40, Strength = 6, Damage = 10 };
             diablo.MaxHealth = diablo.Health;
             return diablo;
         }
@@ -28,19 +28,20 @@ namespace FirstSteps.RPG.Heroes
         {
             double healthPercent = (double)Health / MaxHealth;
 
-            if (healthPercent <= 0.7)
+            double reducedDamage = 1 - healthPercent;
+
+            if(reducedDamage > 0)
             {
-                double reducedDamage = 0.7;
-                return (int)(Damage * reducedDamage);
+                return (int)(Damage * healthPercent);
             }
             else
             {
                 return Damage;
             }
-        }     
+        }
         public int TakeDamage(int damage)
         {
-            return Health -= damage;          
+            return Health -= damage;
         }
     }
 }
